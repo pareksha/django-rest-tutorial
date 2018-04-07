37c6bfb88782e667cf214d5df06719c265174801
pareksha <pareksha.manchanda@gmail.com>
2018-04-07 17:42:56 +0530

    Using ViewSets with routers

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,19 +4,10 @@ from snippets.serializer import UserSerializer, SnippetSerializer
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.decorators import api_view
 from rest_framework.decorators import action
 from rest_framework import renderers
 from rest_framework.renderers import StaticHTMLRenderer
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-    'users': reverse('user-list', request=request, format=format),
-    'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
