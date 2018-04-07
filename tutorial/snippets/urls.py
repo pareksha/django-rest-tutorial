dca611df901a8901308ef1e8fb5adcaf97addd9f
pareksha <pareksha.manchanda@gmail.com>
2018-04-07 17:32:48 +0530

    Using ViewSets but defining every url

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,14 +1,39 @@
 from django.urls import path
-from snippets import views
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf.urls import include
+from snippets.views import UserViewSet, SnippetViewSet, api_root
+from rest_framework import renderers
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 urlpatterns = format_suffix_patterns([
-    url(r'^$', views.api_root),
-    url(r'^snippets/$', views.snippet_list, name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail, name='snippet-detail'),
-    url(r'^users/$', views.user_list, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail, name='user-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.get_snippet_highlight, name='snippet-highlight')
+    url(r'^$', api_root),
+    url(r'^snippets/$', snippet_list, name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
+    url(r'^users/$', user_list, name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight')
 ])
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,15 +1,15 @@
-from snippets.models import Snippet
-from snippets.serializer import SnippetSerializer
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes, renderer_classes
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from snippets.serializer import UserSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework import viewsets
 from django.contrib.auth.models import User
-from rest_framework.reverse import reverse
+from snippets.serializer import UserSerializer, SnippetSerializer
+from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.decorators import action
+from rest_framework import renderers
 from rest_framework.renderers import StaticHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -18,56 +18,19 @@ def api_root(request, format=None):
     'snippets': reverse('snippet-list', request=request, format=format)
     })
 
-@api_view(['POST', 'GET'])
-@permission_classes((IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly))
-def snippet_list(request, format=None):
-    if request.method == 'GET':
-        serializer = SnippetSerializer(Snippet.objects.all(), many=True, context={'request': request})
-        return Response(serializer.data)
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    elif request.method == 'POST':
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-@permission_classes((IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly))
-def snippet_detail(request, pk, format=None):
-    snippet = get_object_or_404(Snippet, pk=pk)
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet, context={'request': request})
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET'])
-def user_list(request, format=None):
-    if request.method == 'GET':
-        serializer = UserSerializer(User.objects.all(), many=True, context={'request': request})
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def user_detail(request, pk, format=None):
-    user = get_object_or_404(User, pk=pk)
-
-    if request.method == 'GET':
-        serializer = UserSerializer(user, context={'request': request})
-        return Response(serializer.data)
-
-@api_view(['GET'])
-@renderer_classes((StaticHTMLRenderer,))
-def get_snippet_highlight(request, pk, format=None):
-    snippet = get_object_or_404(Snippet, pk=pk)
-    return Response(snippet.highlighted)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
